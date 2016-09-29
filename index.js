13aab79e1b458fef8679ec40ee5311a5afc93944
Irsyad <irsyad.muhd@gmail.com>
2016-09-29 22:58:30 +0800

    set up done ejs

---
+++ index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+
+
+app.use(express.static(__dirname + '/public/'));
+
+app.set('view engine', 'ejs');
+
+app.use(require('morgan')('dev'));
+
+app.get('/', function(req, res) {
+  res.render('home');
+});
+
+// app.get('/about', function(req, res) {
+//   res.render('about');
+// });
+//
+// app.get('/package', function(req, res) {
+//   res.render('package');
+// });
+//
+// app.get('/site', function(req, res) {
+//   res.render('site');
+// });
+//
+// app.get('/contact', function(req, res) {
+//   res.render('contact');
+// });
+
+var server = app.listen(process.env.PORT || 3000);
