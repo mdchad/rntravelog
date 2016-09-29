b7a63b756b368c77e9956e3655a2e7e81e207498
Irsyad <irsyad.muhd@gmail.com>
2016-09-30 00:56:14 +0800

    done laying out home

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ app.get('/', function(req, res) {
   res.render('home');
 });
 
-// app.get('/about', function(req, res) {
-//   res.render('about');
-// });
-//
+app.get('/about', function(req, res) {
+  res.render('about');
+});
+
 // app.get('/package', function(req, res) {
 //   res.render('package');
 // });
