c96bd11db5ab7e02b89f1900a0461e04e7a16ffc
Irsyad <irsyad.muhd@gmail.com>
2016-09-29 23:05:12 +0800

    add layout

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var express = require('express');
+var ejsLayouts = require('express-ejs-layouts');
 var app = express();
 
 
 app.use(express.static(__dirname + '/public/'));
 
 app.set('view engine', 'ejs');
+app.use(ejsLayouts);
 
 app.use(require('morgan')('dev'));
 
